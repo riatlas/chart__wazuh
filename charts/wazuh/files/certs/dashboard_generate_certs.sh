90a990efc2976a716efb58e2e0497fdba244d406
Roberto Iannone <roberto.iannone@riatlas.it>
2024-07-11 17:24:42 +0200

    regenerate certs for new version

--- charts/wazuh/files/certs/dashboard_generate_certs.sh
+++ charts/wazuh/files/certs/dashboard_generate_certs.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-openssl req -x509 -batch -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out cert.pem
+openssl req -x509 -batch -nodes -days 3650 -newkey rsa:2048 -keyout key.pem -out cert.pem
